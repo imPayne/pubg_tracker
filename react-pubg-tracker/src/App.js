6aeef43916094ede63c1eb8d9689d832752a19e4
imPayne <hugo.monchablon@epitech.eu>
2023-01-15 18:12:37 +0100

    added chakra

---
+++ react-pubg-tracker/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { PlayerProvider } from './context/playerContext';
+import InputPlayerName from './components/inputPlayerName';
+
+function App() {
+    return (
+        <PlayerProvider>
+            <InputPlayerName />
+        </PlayerProvider>
+    );
+}
+
+export default App;
