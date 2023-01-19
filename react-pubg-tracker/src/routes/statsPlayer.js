d0634f12bbb0615ce94f12bbf84ff2cc90b0afb2
imPayne <hugo.monchablon@epitech.eu>
2023-01-20 00:36:32 +0100

    added display ranked image

--- react-pubg-tracker/src/routes/statsPlayer.js
+++ react-pubg-tracker/src/routes/statsPlayer.js
@@ -2,7 +2,7 @@ import { PlayerProvider } from '../context/playerContext';
 import { Link, useNavigate } from "react-router-dom";
 import { PlayerContext } from '../context/playerContext';
 import { useContext, useState, useEffect } from 'react'
-import { Input, Button, Heading, Box, Container, Text, SliderProvider } from '@chakra-ui/react'
+import { Input, Button, Heading, Box, Container, Text, SliderProvider, Image } from '@chakra-ui/react'
 
 function StatsPlayer() {
     const playerContext = useContext(PlayerContext);
@@ -12,9 +12,12 @@ function StatsPlayer() {
             <Container mt={25}>
                 <Box mt={5} p={7} borderRadius={7} backgroundColor='#F0F0F0'>
                     <Heading pl={28}>Pubg Stats:</Heading>
-                    <Heading size="md" mt={3}>{playerContext.playerName}</Heading>
+                    <Heading size="md" mt={3} mb={5}>{playerContext.playerName}</Heading>
+                    <Box boxSize='8rem'>
+                        <Image src={"/images/" + playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].currentTier.tier + playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].currentTier.subTier + ".png"}></Image>
+                    </Box>
                     <Text>Rank: {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].currentTier.tier} {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].currentTier.subTier}</Text>
-                    <Text>Rating: {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].currentRankPoint}</Text>
+                    <Text>Rating: {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].currentRankPoint} RP</Text>
                     <Text>win%: {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].winRatio * 100}</Text>
                     <Text>kills: {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].kills}</Text>
                     <Text>deaths: {playerContext.rankedStats.data.attributes.rankedGameModeStats["squad-fpp"].deaths}</Text>
